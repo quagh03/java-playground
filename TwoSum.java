bcd48b71532f1542c486f4651a1561e505ab211c
quagh03 <03.quanghuy.le@gmail.com>
2023-11-16 15:08:57 +0700

    two-sum

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int counterpart = target - nums[i];
+            if(!map.containsKey(counterpart)){
+                map.put(nums[i], i);
+            }else{
+                return new int[] {map.get(counterpart), i};
+            }
+        }
+        return new int[0];
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
