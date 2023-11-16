dbb445453e8ee88a74accea9ddee1260092b948e
quagh03 <03.quanghuy.le@gmail.com>
2023-11-16 21:11:28 +0700

    function to check validity of the blockchain

--- SimpleBlockChainStructure/HLQ.java
+++ SimpleBlockChainStructure/HLQ.java
@@ -5,6 +5,25 @@ import java.util.ArrayList;
 public class HLQ {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
+    public static Boolean isChainValid() {
+        for(int i=1; i < blockchain.size(); i++) {
+            Block currentBlock = blockchain.get(i);
+            Block previousBlock = blockchain.get(i-1);
+
+            //compare registered hash and calculated hash:
+            if(!currentBlock.Hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");         
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.Hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args){
         blockchain.add(new Block("Faculty of Information Technology", null));
         blockchain.add(new Block("Hanoi University of Civil Engineering", 
@@ -17,6 +36,8 @@ public class HLQ {
                 .get(blockchain.size() - 1 )
                 .Hash
         ));
+        
+        System.out.println("Blockchain is Valid: " + isChainValid());
 
         for(int i = 0; i < blockchain.size(); i++){
             System.out.println("Block " + i);
